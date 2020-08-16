7c806a547da7733950ad0394ecdbbfc21c39259d
Aron Phong <aronphong@gmail.com>
2020-08-16 11:45:52 -0700

    Update depecrated function

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 
 const mongoose = require('mongoose');
 const mongoDB = process.env.MONGO_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
